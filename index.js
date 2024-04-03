0e361ca80b3f41c82b7989a2ef9739cd62717f05
Mungai <mungaij697@gmail.com>
2024-04-03 13:56:13 +0300

    flow

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Distance){if (Distance <= 400){
+  return"This one is on me!";
+}else if (Distance > 400 && Distance <= 2000){
+  return "That will be twenty bucks.";
+}else if (Distance > 2000 && Distance <= 2500) {
+  return"I will gladly take your thirty bucks."
+}else{
+  return "No can do.";
+}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(City){
+  return City === "NYC" ? "Ok, sounds good." :
+  "No go.";
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
+  switch (Tip) {
+    case "generous":
+      return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+        default:
+          return "Bye."
+  }
   // Write your code here!
 }
